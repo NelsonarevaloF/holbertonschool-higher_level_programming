469154898bc620372685971e39f5ccfb23a65fb0
Nelson Arevalo <nelson.arevalo.tecni@gmail.com>
2020-12-10 20:32:47 +0000

    added task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-for i in range(9):
-    for j in range(i + 1, 10):
-        if i != 8 or j != 9:
-            print("{}{}".format(i, j), end=", ")
-        else:
-            print("{}{}".format(i, j))
+for i in range(10):
+    j = i
+    while j < 10:
+        if i != j and (j != 9 or i != 8):
+            print("{}{}, ".format(i, j), end='')
+        j += 1
+print("89")
