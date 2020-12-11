edead978b566c098b68203666b558bd6ccae4951
Nelson Arevalo <nelson.arevalo.tecni@gmail.com>
2020-12-11 03:26:51 +0000

    added task 0 task 1 and task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    number_argv = len(argv) - 1
+    if number_argv == 0:
+        print("{} arguments.".format(number_argv))
+    elif number_argv == 1:
+        print("{} argument:".format(number_argv))
+    else:
+        print("{} arguments:".format(number_argv))
+    if number_argv > 0:
+        for i in range(1, number_argv + 1):
+            print("{:d}. {:s}".format(i, argv[i]))
