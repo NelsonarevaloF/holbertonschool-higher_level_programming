fddd5a08f9a5fe81ae0bc866da91baa664675f95
Nelson Arevalo <nelson.arevalo.tecni@gmail.com>
2020-12-18 05:13:16 +0000

    task 1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
-def square_matrix_simple(matrix=[]):
-    matrixc = []
-    for n in matrix:
-        matrixf = list(map(lambda x: x ** 2, n))
-        matrixc.append(matrixf)
-    return matrixc
+def search_replace(my_list, search, replace):
+    return list(map(lambda x : replace if (x == search) else x, my_list))
