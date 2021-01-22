c44d9cc8b3ce42d30aaef51e353ed1e4daa2f18e
Nelson Arevalo <nelson.arevalo.tecni@gmail.com>
2021-01-22 05:57:57 +0000

    fixed: change the struct of program, the previous one

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -9,10 +9,9 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
 path = "add_item.json"
 try:
-    f = open(path, 'x')
-    save_to_json_file([], path)
+    f_return = load_from_json_file(path)
 except:
-    list_return = load_from_json_file(path)
-    for arg in sys.argv[1:]:
-        list_return.append(arg)
-    save_to_json_file(list_return, path)
+    f_return = []
+for item in sys.argv[1:]:
+    f_return.append(item)
+save_to_json_file(f_return, path)
