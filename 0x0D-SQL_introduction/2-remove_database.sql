386ed2d525db07b36e73e57616a6cf882e389729
Nelson Arevalo <nelson.arevalo.tecni@gmail.com>
2021-03-02 03:48:48 +0000

    added: task 2 and task 3

---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,3 @@
+-- deletes the database hbtn_0c_0 if exists
+-- deletes a database
+DROP DATABASE IF EXISTS hbtn_0c_0
---
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -0,0 +1,3 @@
+-- list all the tables of a database in the MySQL server.
+-- list all the tables
+SHOW tables;
