e67963d9cedc08cf6ef3164f0f10e7f2001cd561
Nelson Arevalo <nelson.arevalo.tecni@gmail.com>
2021-03-15 02:06:56 +0000

    added: task 10

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * check_cycle - checks if a list has a cycle or not
+ * @list: pointer to list structure
+ * Return: 0 if list has no cycle, else 1.
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *head = list;
+
+	if (list)
+	{
+		while (list && head && head->next)
+		{
+			list = list->next;
+			head = head->next->next;
+			if (list == head)
+				return (1);
+		}
+	}
+	return (0);
+}
