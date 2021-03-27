c158ec80cf481859a5491240c52a8c80af6749ea
Nelson Arevalo <nelson.arevalo.tecni@gmail.com>
2021-03-27 23:02:58 +0000

    added: task 3

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+
+'''this module sets connection with the BD and shows the date
+that start witn N'''
+
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    arg = sys.argv
+    db = MySQLdb.connect(host="localhost", user=arg[1],
+                         passwd=arg[2], db=arg[3])
+    cur = db.cursor()
+    filter_name = arg[4]
+
+    cur.execute("SELECT * FROM states WHERE name=%s"
+                "ORDER BY states.id ASC;", (filter_name,))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
