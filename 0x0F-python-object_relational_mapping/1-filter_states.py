b8fa61bf3efa769b0bdddfcf4659ef10cf291081
Nelson Arevalo <nelson.arevalo.tecni@gmail.com>
2021-03-27 09:45:22 +0000

    added: task 1

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+
+'''this module sets connection with the BD and shows the date
+that start witn N'''
+
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    arg = sys.argv
+    db = MySQLdb.connect(host="localhost", user=arg[1],
+                         passwd=arg[2], db=arg[3])
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%'")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
