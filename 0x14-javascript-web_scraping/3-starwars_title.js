8cb774755d316ddd40c2b5e9c17ac40e4330c48e
Nelson Arevalo <nelson.arevalo.tecni@gmail.com>
2021-05-08 05:06:14 +0000

    added: task 3 my version

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,5 +1,9 @@
 #!/usr/bin/node
 const request = require('request');
-request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function(err, res, body) {
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, body) {
+  if (err) {
+    console.error(err);
+    return;
+  }
   console.log(JSON.parse(body).title);
 });
